20a2c64657d775b59c4c9c1ff2a4fab68c53a91f
Douglas Ronne <vextorspace@gmail.com>
2024-11-14 08:35:28 -0800

    Add sys import and exit on test failure

--- test_suite.py
+++ test_suite.py
@@ -1,5 +1,6 @@
 import unittest
 from ai_coder import TestAiCoder
+import sys
 
 def suite():
     suite = unittest.TestSuite()
@@ -12,3 +13,4 @@ def suite():
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
     result = runner.run(suite())
+    sys.exit(not result.wasSuccessful())
