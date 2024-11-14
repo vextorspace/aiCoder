d2a8b37af94a3c7864d1cdc63c31cbb45fb8ed96
Douglas Ronne <vextorspace@gmail.com>
2024-11-14 11:30:28 -0800

    Add TestAi to test suite execution

--- test_suite.py
+++ test_suite.py
@@ -1,6 +1,7 @@
 import unittest
 from ai_coder import TestAiCoder
 from ai_diff_gen import TestAiDiffGen
+from ai import TestAi
 
 import sys
 
@@ -10,6 +11,8 @@ def suite():
 
     suite.addTests(loader.loadTestsFromTestCase(TestAiCoder))
     suite.addTests(loader.loadTestsFromTestCase(TestAiDiffGen))
+    suite.addTests(loader.loadTestsFromTestCase(TestAi))
+
     return suite
 
 if __name__ == '__main__':
